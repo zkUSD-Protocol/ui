b73ca3b5254fa0aec9c35722920c0d8dcb8c24c2
Charlie Mack <mack.cee@gmail.com>
2025-08-04 09:37:17 +0200

    redeploy

--- src/lib/db/mongodb.ts
+++ src/lib/db/mongodb.ts
@@ -16,9 +16,9 @@ interface Cached {
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-// if (!MONGODB_URI) {
-//   throw new Error("Please define the MONGODB_URI environment variable");
-// }
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
 
 let cached = global.mongoose as Cached;
 
